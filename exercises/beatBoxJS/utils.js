211ce8495b0618d2a4765579903f34e9d03365ad
Emmanuel <immanuelidun10@gmail.com>
2020-06-20 00:22:46 -0600

    done

--- exercises/beatBoxJS/utils.js
+++ exercises/beatBoxJS/utils.js
@@ -4,6 +4,14 @@
  * HINT: Create a play function to play the audio if called 
  */
 class Beat {
+    constructor(audioSrc) {
+        this.myAudio = new Audio(audioSrc);
+    }
+
+    Play = () => {
+        this.myAudio.currentTime = 0;
+        this.myAudio.play();
+    }
 }
 
 
@@ -13,23 +21,39 @@ class Beat {
  */
 class Button {
     constructor(color, keyCode){
+        this.color = color;
+        this.keyCode = keyCode;
+        this.setButtonColorInHTML();
+         this.setTransition();
     }
 
     /**
      * Set the button color based on color specified
+     * 
      */
+    setTransition = () => {
+        document.addEventListener('transitionend', this.deselect);
+    }
+    
+
+
     setButtonColorInHTML = () => {
+        document.getElementById(this.keyCode).style.borderColor = this.color;
     }
 
     /**
      * Select function to set the background color and boxShadow
      */
     select = () => {
+        document.getElementById(this.keyCode).style.backgroundColor = this.color;
+        document.getElementById(this.keyCode).style.boxShadow = `0px 0px 17px 0px ${this.color}`;
     }
 
     /**
      * Deselect function to reset background color and boxShadow
      */
     deselect = () => {
+        document.getElementById(this.keyCode).style.backgroundColor = "transparent";
+        document.getElementById(this.keyCode).style.boxShadow = "none";
     }
 }
